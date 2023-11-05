e50fb14fa76324da1085029677beac224304d0f2
0xcfff <0xcfff@users.noreply.github.com>
2023-11-05 23:23:45 +0400

    Update hostsctl.rb

--- Formula/hostsctl.rb
+++ Formula/hostsctl.rb
@@ -1,6 +1,5 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Hostsctl < Formula
   desc "A small tool to manage aliases in /etc/hosts file"
   homepage "https://github.com/0xcfff/hostsctl"
@@ -22,15 +21,6 @@ class Hostsctl < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test hostsctl`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "#{bin}/hostsctl", "database", "print"
+    system "#{bin}/hostsctl", "version"
   end
 end
