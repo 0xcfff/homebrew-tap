960d457320c1caf400b9f95779156733e214c602
Mikhail <0xcfff@gmail.com>
2023-11-05 23:34:05 +0400

    Fix formatting

--- Formula/hostsctl.rb
+++ Formula/hostsctl.rb
@@ -1,17 +1,14 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
 class Hostsctl < Formula
-  desc "A small tool to manage aliases in /etc/hosts file"
+  desc "Small tool to manage aliases in /etc/hosts file"
   homepage "https://github.com/0xcfff/hostsctl"
   url "https://github.com/0xcfff/hostsctl/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "c6c6306f70b1b4a780d8b61186b59b48ce5621e6f7639ab6902e5365a3f93d55"
   license "Apache-2.0"
-  version "0.0.1"
 
   depends_on "go" => :build
 
   def install
-  
+
     ldflags = %W[
       -s -w
       -X main.VERSION=#{version}
