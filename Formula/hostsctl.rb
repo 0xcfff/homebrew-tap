1c3431dcbee9152487b6e8061b8d46baa57d4c44
0xcfff <0xcfff@gmail.com>
2026-08-18 07:56:48 +0000

    hostsctl: bump to v0.0.2

--- Formula/hostsctl.rb
+++ Formula/hostsctl.rb
@@ -1,8 +1,8 @@
 class Hostsctl < Formula
   desc "Small tool to manage aliases in /etc/hosts file"
   homepage "https://github.com/0xcfff/hostsctl"
-  url "https://github.com/0xcfff/hostsctl/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "c6c6306f70b1b4a780d8b61186b59b48ce5621e6f7639ab6902e5365a3f93d55"
+  url "https://github.com/0xcfff/hostsctl/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "36112328ea05876193d11c94da47b0ae6c4a5e7515b7408d6c4e55aa469ab4a5"
   license "Apache-2.0"
 
   depends_on "go" => :build
